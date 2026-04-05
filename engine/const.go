4a94d9a38d39bb41178154ff176765f1f880bec3
Björn Petersen <git@bjoernpetersen.net>
2026-04-05 12:37:55 +0200

    ci: release using GH Actions

--- engine/const.go
+++ engine/const.go
@@ -54,7 +54,7 @@ write_files:
       default:
         enable_ipv6: true
 runcmd:
-  - sh -xc "cd /root; docker compose up -d"
+  - sh -xc "cd /root; docker compose up"
 
 final_message: "The system is finally up, after $UPTIME seconds"
 ` // editorconfig-checker-enable
